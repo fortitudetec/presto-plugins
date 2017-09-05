bed60202a0e7392381fec1d7462f49ffed8c1ea2
Aaron McCurry <amccurry@gmail.com>
2017-09-05 12:37:26 -0400

    Changing the way that hdfs config is loaded.

--- spreadsheet-storage-handler/src/main/java/com/fortitudetec/presto/spreadsheets/SpreadsheetConnectorFactory.java
+++ spreadsheet-storage-handler/src/main/java/com/fortitudetec/presto/spreadsheets/SpreadsheetConnectorFactory.java
@@ -16,22 +16,31 @@
  */
 package com.fortitudetec.presto.spreadsheets;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.HdfsConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.facebook.presto.spi.ConnectorHandleResolver;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.connector.Connector;
 import com.facebook.presto.spi.connector.ConnectorContext;
 import com.facebook.presto.spi.connector.ConnectorFactory;
+import com.fortitudetec.presto.BaseErrorCode;
 
 public class SpreadsheetConnectorFactory implements ConnectorFactory {
+
+  private static final String HDFS_SITE_XML = "hdfs-site.xml";
+
+  private static final String CORE_SITE_XML = "core-site.xml";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SpreadsheetConnectorFactory.class);
 
@@ -39,19 +48,11 @@ public class SpreadsheetConnectorFactory implements ConnectorFactory {
   private static final String SUBDIR = "subdir";
   private static final String BASEPATH = "basepath";
 
-  private final Configuration _configuration;
+  private final AtomicReference<Configuration> _configuration = new AtomicReference<Configuration>(null);
   private final SpreadsheetHandleResolver _handleResolver;
 
   public SpreadsheetConnectorFactory(ClassLoader classLoader) {
-    _configuration = new HdfsConfiguration();
-    // This is kind of stupid but because FileSystem only loads built in types
-    // from the system classloader the DistributedFileSystem won't load if it's
-    // in a non system class loader.
-    ServiceLoader<FileSystem> serviceLoader = ServiceLoader.load(FileSystem.class);
-    for (FileSystem fs : serviceLoader) {
-      LOGGER.info("Loading filesystem type {} class {}", fs.getScheme(), fs.getClass());
-      _configuration.setClass("fs." + fs.getScheme() + ".impl", fs.getClass(), FileSystem.class);
-    }
+
     _handleResolver = new SpreadsheetHandleResolver();
   }
 
@@ -62,6 +63,7 @@ public class SpreadsheetConnectorFactory implements ConnectorFactory {
 
   @Override
   public Connector create(String connectorId, Map<String, String> config, ConnectorContext context) {
+    Configuration configuration = getConfiguration(config.get("hdfs.conf"));
     Path basePath = new Path(config.get(BASEPATH));
     String spreadsheetSubDir = config.get(SUBDIR);
     String useFileCacheStr = config.get(USE_FILE_CACHE);
@@ -69,7 +71,37 @@ public class SpreadsheetConnectorFactory implements ConnectorFactory {
     if (useFileCacheStr != null) {
       useFileCache = Boolean.parseBoolean(useFileCacheStr);
     }
-    return new SpreadsheetConnector(_configuration, basePath, spreadsheetSubDir, useFileCache);
+    return new SpreadsheetConnector(configuration, basePath, spreadsheetSubDir, useFileCache);
+  }
+
+  private Configuration getConfiguration(String path) {
+    synchronized (_configuration) {
+      Configuration configuration = new Configuration(true);
+      addFileIfExists(path + "/" + CORE_SITE_XML, configuration);
+      addFileIfExists(path + "/" + HDFS_SITE_XML, configuration);
+      // This is kind of stupid but because FileSystem only loads built in types
+      // from the system classloader the DistributedFileSystem won't load if
+      // it's
+      // in a non system class loader.
+      ServiceLoader<FileSystem> serviceLoader = ServiceLoader.load(FileSystem.class);
+      for (FileSystem fs : serviceLoader) {
+        LOGGER.info("Loading filesystem type {} class {}", fs.getScheme(), fs.getClass());
+        configuration.setClass("fs." + fs.getScheme() + ".impl", fs.getClass(), FileSystem.class);
+      }
+      _configuration.set(configuration);
+      return _configuration.get();
+    }
+  }
+
+  private void addFileIfExists(String path, Configuration configuration) {
+    File file = new File(path);
+    if (file.exists()) {
+      try {
+        configuration.addResource(new FileInputStream(file));
+      } catch (FileNotFoundException e) {
+        throw new PrestoException(BaseErrorCode.CONFIG_ERROR, path + " not found.");
+      }
+    }
   }
 
   @Override
